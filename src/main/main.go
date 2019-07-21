2bc52bcb965d62d93c5c34ba8512ba492baf2ccf
miku <lwqmyself@gmail.com>
2019-07-21 18:31:23 +0800

    测试

---
+++ src/main/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("handle hello")
+	fmt.Fprintf(w, "hello ")
+}
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("handle login")
+	fmt.Fprintf(w, "login")
+}
+func main() {
+	http.HandleFunc("/", Hello)
+	http.HandleFunc("/user/login", login)
+	err := http.ListenAndServe("0.0.0.0:8880", nil)
+	if err != nil {
+		fmt.Println("http listen failed")
+	}
+}
